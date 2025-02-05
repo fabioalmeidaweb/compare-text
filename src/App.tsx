8755b227c16023b398d78bf38ce84b77f19ecf98
Fabio Almeida <fabio.natito@gmail.com>
2025-02-05 14:39:22 -0300

    (feat) change styles and add expanded textarea

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { Change } from "diff";
-import { TextArea } from "./components/TextArea";
 import classNames from "classnames";
 import sanitizeHtml from "sanitize-html";
+import { Change } from "diff";
+
+import { TextArea } from "./components/TextArea";
 import { escapeHtml } from "./utils/scapeHtml";
 
 const App: React.FC = () => {
@@ -29,55 +30,68 @@ const App: React.FC = () => {
   };
 
   return (
-    <main className="flex flex-col gap-5 w-full h-screen  p-10 font-mono">
+    <main className="flex flex-col gap-5 w-full min-h-screen bg-slate-800 lg:p-10">
       <header className="m-10">
-        <h1 className="text-5xl font-bold text-center">Compare Text</h1>
+        <h1 className="text-5xl font-bold text-center text-slate-100">
+          Compare Text
+        </h1>
       </header>
-      <section className="flex gap-5 m-10">
-        <TextArea text={text1} onChange={setText1} />
-        <TextArea text={text2} onChange={setText2} />
+
+      <section className="flex flex-col lg:flex-row gap-5 m-10 font-mono">
+        <TextArea
+          text={text1}
+          expanded={diffResult.length === 0}
+          onChange={setText1}
+        />
+        <TextArea
+          text={text2}
+          expanded={diffResult.length === 0}
+          onChange={setText2}
+        />
       </section>
 
       <section className="flex justify-center gap-5 m-10">
         <button
-          className="border-2 border-solid rounded-lg text-2xl w-32"
+          className="border-2 border-solid rounded-lg text-2xl text-slate-400 bg-slate-700 border-slate-400 w-32 p-1"
           onClick={handleCompare}
         >
           Compare
         </button>
         <button
-          className="border-2 border-solid rounded-lg text-2xl w-32"
+          className="border-2 border-solid rounded-lg text-2xl text-slate-400 bg-slate-700 border-slate-400 w-32 p-1"
           onClick={handleClean}
         >
           Reset
         </button>
       </section>
 
-      <section className="flex justify-center m-10">
-        <pre className="bg-zinc-200 w-300 h-auto rounded-lg p-5">
-          {diffResult.map((part, index) => {
-            return (
-              <span
-                key={index}
-                className={classNames(
-                  part.added ? "bg-green-200" : "",
-                  part.removed ? "bg-rose-200" : "",
-                  "font-mono",
-                  "whitespace-pre-wrap"
-                )}
-                dangerouslySetInnerHTML={{
-                  __html: sanitizeHtml(
-                    escapeHtml(part.value).replace(/\n/g, "<br />"),
-                    {
-                      allowedTags: ["br"],
-                    }
-                  ),
-                }}
-              />
-            );
-          })}
-        </pre>
-      </section>
+      {diffResult.length > 0 && (
+        <section className="flex justify-center m-10">
+          <pre className="bg-slate-900 text-slate-50 w-300 h-auto rounded-lg p-5">
+            {diffResult.map((part, index) => {
+              return (
+                <span
+                  key={index}
+                  className={classNames(
+                    part.added ? "bg-lime-900" : "",
+                    part.removed ? "bg-red-900" : "",
+                    "font-mono",
+                    "whitespace-pre-wrap"
+                  )}
+                  dangerouslySetInnerHTML={{
+                    __html: sanitizeHtml(
+                      escapeHtml(part.value).replace(/\n/g, "<br />"),
+                      {
+                        allowedTags: ["br"],
+                      }
+                    ),
+                  }}
+                />
+              );
+            })}
+          </pre>
+        </section>
+      )}
     </main>
   );
 };
--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,12 +1,20 @@
+import classNames from "classnames";
+
 type TextAreaProps = {
   text: string;
+  expanded: boolean;
   onChange: (text: string) => void;
 };
 
-export function TextArea({ text, onChange }: TextAreaProps) {
+export function TextArea({ text, expanded, onChange }: TextAreaProps) {
+  const className = classNames(
+    "border-2 border-solid bg-slate-700 text-slate-50 border-slate-700 hover:border-slate-800 focus:border-slate-900 focus:outline-none w-full h-64 rounded-lg p-5",
+    { "h-50": !expanded, "h-200": expanded }
+  );
+
   return (
     <textarea
-      className="border-2 border-solid border-zinc-200 hover:border-zinc-300 focus:border-zinc-400 focus:outline-none w-full h-64 rounded-lg p-5"
+      className={className}
       value={text}
       onChange={(e) => onChange(e.target.value)}
     />
